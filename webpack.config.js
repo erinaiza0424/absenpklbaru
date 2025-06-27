2f0031baa2d8988dda6de528d95491fe8052afc9
erinaiza0424 <laluadha45@guru.smk.belajar.id>
2025-06-27 21:30:42 +0700

    fix: final webpack config for Vercel deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx', // pastikan file entry sesuai dengan struktur proyekmu
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
     publicPath: '/',
     clean: true,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
@@ -26,17 +27,24 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[hash][ext][query]'
-        }
-      }
+          filename: 'images/[name].[contenthash][ext]',
+        },
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'dist'),
     },
     port: 3001,
     historyApiFallback: true,
+    open: true,
     proxy: {
       '/absen': 'http://localhost:3000',
       '/absensi': 'http://localhost:3000',
